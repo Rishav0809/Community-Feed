d853c61c42dc455e3ca8923413e2ec91329d40ce
amanchoudhary18 <aman.choudhary9785@gmail.com>
2022-07-11 20:25:41 +0530

    news-feed

--- ejs_integration/index.js
+++ ejs_integration/index.js
@@ -1,38 +1,41 @@
-var express = require('express');
+const decoder = require("jwt-decode");
+var express = require("express");
+const bodyParser = require("body-parser");
+
+var admin = require("firebase-admin");
+
 var app = express();
 
-// app.get('/', function (req, res) {
-//    res.send('Hello World');
-// })
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+console.log(__dirname);
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
- console.log(__dirname);
+app.get("/", (req, res) => {
+  res.render("index", { title: "News Feed" });
+});
 
+app.get("/community", (req, res) => {
+  res.render("community", { title: "Community Feed" });
+});
 
+app.get("/sip", (req, res) => {
+  res.render("sip", { title: "SIP Calculator" });
+});
 
-app.get('/', (req, res) => {
-    res.render('index', {title: 'News Feed'});
-  });
+app.get("/fd", (req, res) => {
+  res.render("fd", { title: "FD Calculator" });
+});
 
+app.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
+});
 
-  app.get('/community', (req, res) => {
-    res.render('community', {title: 'Community Feed'});
-  });
-
-  
-
-  app.get('/sip', (req, res) => {
-    res.render('sip', {title: 'SIP Calculator'});
-  });
-
-  app.get('/fd', (req, res) => {
-    res.render('fd', {title: 'FD Calculator'});
-  });
-
+app.post("/login", (req, res) => {
+  console.log(req.credential);
+});
 
 var server = app.listen(3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Listening at http://%s:%s", host, port)
-})
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Listening at http://%s:%s", host, port);
+});
